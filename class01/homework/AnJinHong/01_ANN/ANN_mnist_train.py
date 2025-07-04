f90218f4f4c36bd3722e2bd7a797b8036918a230
anjinhong <dkswlsghd369@naver.com>
2025-07-04 10:50:14 +0900

    homework

---
+++ class01/homework/AnJinHong/01_ANN/ANN_mnist_train.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+
+(image_train, label_train), (image_test, label_test) = mnist.load_data()
+image_train, image_test = image_train / 255.0, image_test / 255.0
+
+class_names=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+plt.figure(figsize=(10,10))
+for i in range(10):
+    plt.subplot(3,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(image_train[i])
+    plt.xlabel(class_names[label_train[i]])
+plt.show()
+
+
+#ANN
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile( optimizer='adam',
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'],)
+model.fit(image_train, label_train, epochs=10, batch_size=10)
+model.summary()
+model.save('mnist.h5')
+
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+model =tf.keras.models.load_model('./mnist.h5')
+mnist = tf.keras.datasets.mnist
+(image_train, label_train), (image_test, label_test) =mnist.load_data()
+
+image_ftain, image_test =image_train /255.0, image_test / 255.0
+
+num = 10
+predict = model.predict(image_test[:num])
+print(label_test[:num])
+print(" * Prediction, ", np.argmax(predict, axis = 1))
---
+++ class01/homework/AnJinHong/02_CNN/CNN_mnist_train.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+mnist = tf.keras.datasets.fashion_mnist
+
+(image_train, label_train), (image_test, label_test) = mnist.load_data()
+image_train, image_test = image_train / 255.0, image_test / 255.0
+
+class_names=['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+ 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+print(label_train)
+
+plt.figure(figsize=(10,10))
+for i in range(10):
+    plt.subplot(3,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(image_train[i], cmap=cm.gray)
+    plt.xlabel(class_names[label_train[i]])
+plt.show()
+
+
+
+#homework#2
+#CNN
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation ='relu', input_shape=(28,28,1)))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation ='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation ='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Flatten())
+#ANN
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile( optimizer='adam',
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'],)
+model.fit(image_train, label_train, epochs=10, batch_size=10)
+model.summary()
+model.save('fashion_mnist.keras')
+
+
+# import tensorflow as tf
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# model =tf.keras.models.load_model('./mnist.h5')
+# mnist = tf.keras.datasets.mnist
+# (image_train, label_train), (image_test, label_test) =mnist.load_data()
+
+# image_ftain, image_test =image_train /255.0, image_test / 255.0
+
+# num = 10
+# predict = model.predict(image_test[:num])
+# print(label_test[:num])
+# print(" * Prediction, ", np.argmax(predict, axis = 1))
